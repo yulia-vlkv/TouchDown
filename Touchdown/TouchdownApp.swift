ceeb9876ee2e9911429ac408cd6fe8c7b7b52e2c
Iuliia Vlkv <yu.wlkw@gmail.com>
2022-07-09 09:26:10 +0200

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Iuliia Volkova on 09.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
